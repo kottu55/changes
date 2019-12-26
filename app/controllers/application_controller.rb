b7515750fb6b1f4b35b3aca4fcac3c28e8d46642
kottu55 <kotsuboon@gmail.com>
2019-12-26 04:20:13 +0000

    chage root

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -15,4 +15,9 @@ class ApplicationController < ActionController::Base
     def after_update_path_for(resource)
          my_page_path
     end
+
+    def after_sign_in_path_for(resource)
+      posts_path
+    end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
-  root 'posts#index'
-  get 'posts/lp' => 'posts#lp'
+  root 'posts#lp'
   resources :users
   resources :categories
   resources :relationships, only: [:create, :destroy]
